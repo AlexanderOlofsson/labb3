279288f9aa6645afa8c81853cf38527fb8933976
AlexanderOlofsson <alex.j.olofsson@gmail.com>
2024-12-17 19:35:01 +0100

    Today I mainly worked in figma etc to solve the final design. Will work more now on the actual code.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Lägg till din Product-komponent på /product */}
         <Route path="/" element={<Product />} />
       </Routes>
     </Router>
